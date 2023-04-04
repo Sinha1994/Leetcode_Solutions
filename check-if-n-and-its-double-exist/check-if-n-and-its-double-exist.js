d5cf19ef1360a2ba0f1c9572d546c254338986e1
Suman Sinha <sinha1194@gmail.com>
2023-04-04 23:30:37 +0530

    Time: 57 ms, Memory: 41.6 MB - LeetHub

---
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function(arr) {
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=i+1;j<arr.length;j++) {
+            if(arr[i] == 2*arr[j] || arr[j] == 2*arr[i])
+            return true;
+        }
+    }
+    return false;
+};

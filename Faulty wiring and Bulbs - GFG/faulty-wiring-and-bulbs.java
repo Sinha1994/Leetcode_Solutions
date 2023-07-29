63af0bb750d9850f6b8ac5f6ba4283ebbae0f2cb
Suman Sinha <sinha1194@gmail.com>
2023-07-29 12:21:34 +0530

    Added solution - LeetHub

---
+++ Faulty wiring and Bulbs - GFG/faulty-wiring-and-bulbs.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution
+{
+    public static int countFlips(int a[], int n)
+    {
+        //code here.
+        int cost = 0;
+        for(int i=0;i<a.length;i++) {
+            if(cost%2 != 0) a[i] = a[i] == 1 ? 0 : 1;
+            if(a[i] == 0) ++cost;
+        }
+        return cost;
+    }
+}
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine().trim());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    int sizeOfArray = Integer.parseInt(br.readLine().trim());
+		    
+		    int arr[] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int index = 0;index < sizeOfArray; index++){
+		        arr[index] = Integer.parseInt(elements[index]);
+		    }
+		    
+		    Solution ob = new Solution();
+		    System.out.println(ob.countFlips(arr,sizeOfArray));
+		}
+	}
+}
+
+
+// } Driver Code Ends
